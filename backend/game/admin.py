01fd882f1cd658aaba2a74e1f77c3af44df4b860
이철희 <dlcjfgmlnasa@naver.com>
2022-11-16 17:09:52 +0900

    Admin 페이지 추가

--- backend/game/admin.py
+++ backend/game/admin.py
@@ -1,3 +1,7 @@
+# -*- coding:utf-8 -*-
 from django.contrib import admin
+from game.models import Game, MiniGame
 
-# Register your models here.
+
+admin.site.register(Game)
+admin.site.register(MiniGame)
--- backend/user/admin.py
+++ backend/user/admin.py
@@ -1,3 +1,6 @@
+# -*- coding:utf-8 -*-
 from django.contrib import admin
+from user.models import User
 
-# Register your models here.
+
+admin.site.register(User)

9a491d311705e4afe6e2f925a6aef552c46f7248
이철희 <dlcjfgmlnasa@naver.com>
2022-11-17 20:53:25 +0900

    Device Admin 추가

--- backend/signals/admin.py
+++ backend/signals/admin.py
@@ -1,3 +1,15 @@
+# -*- coding:utf-8 -*-
 from django.contrib import admin
+from signals.models import Device
 
-# Register your models here.
+
+@admin.register(Device)
+class DeviceAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'device_type',
+        'sampling_rate',
+        'description',
+        'int_dt',
+        'upt_dt'
+    )
